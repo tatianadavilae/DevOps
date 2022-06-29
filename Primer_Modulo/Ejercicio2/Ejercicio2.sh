50e6ccc5820eb7ae3f14b69c1bf2fda5b3388e36
tatianadavilae <tatiana.davila125@gmail.com>
2022-06-29 18:40:48 +0200

    Ejercicio 2 completo

--- Primer_Modulo/Ejercicio2/Ejercicio2.sh
+++ Primer_Modulo/Ejercicio2/Ejercicio2.sh
@@ -6,6 +6,8 @@ mkdir dummy
 mkdir empty
 cd dummy
 touch file{1,2}.txt
-echo "$1" > file1.txt
+if [ "$1" > 0 ] ; then echo "$1" > file1.txt
+else echo 'Que me gusta la bash!!!!' > file1.txt
+fi
 cp file1.txt file2.txt
 mv file2.txt ../empty/
